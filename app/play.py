56e5f0722b381cbe1ad2cd06e42098dc04fe0343
jasonou <jasonou.nosleep@gmail.com>
2021-09-30 22:01:38 -0700

    FIXES-3-11-12: Exit script hotkey, code formatter, default settings

--- app/play.py
+++ app/play.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import os
+import keyboard
 import pyautogui as p
 from random import randint
 from twilio.rest import Client
@@ -11,20 +12,30 @@ actions = []
 cwd = os.getcwd()
 settings_read = False
 
+
 class Settings:
-    replay_loops=1
-    log_comments=True
-    log_actions=True
-    log_debug=False
-    click_delay_min=0.1
-    click_delay_max=0.2
-    notification_delay=30
-    notification_loops=5
+    replay_loops = 1
+    log_comments = True
+    log_actions = True
+    log_debug = False
+    click_delay_min = 0.1
+    click_delay_max = 0.2
+    notification_delay = 30
+    notification_loops = 5
+
+
+def stop():
+    print(f'[[ Exiting... ]]')
+    os._exit(0)
+
 
 settings = Settings()
+keyboard.add_hotkey("ctrl+esc", stop)
+
 
 def log(message):
     print(message)
+
 
 def read():
     filename = f'{cwd}/output/{sys.argv[1]}.txt'
@@ -34,10 +45,12 @@ def read():
     if settings.log_actions:
         log(f'[[ Reading Recording File ]] - [[ Success ]]')
 
+
 def do_screenshot():
     img = p.screenshot()
-    os.makedirs(f'{cwd}/logging', exist_ok=True) 
+    os.makedirs(f'{cwd}/logging', exist_ok=True)
     img.save(f'{cwd}/logging/{time.time()}-PAUSED.PNG')
+
 
 def do_notification():
     account_sid = os.getenv('TWILIO_SID')
@@ -52,11 +65,13 @@ def do_notification():
     if settings.log_debug:
         log(f'[[ Notification Triggered ]]: texting')
 
+
 def do_pause(min_seconds, max_seconds):
-    sleepfor = randint(min_seconds*100, max_seconds*100)/100.0
+    sleepfor = randint(min_seconds * 100, max_seconds * 100) / 100.0
     if settings.log_debug:
         log(f'[[ Sleeping For ]] - [[ {str(sleepfor)} ]]')
     time.sleep(sleepfor)
+
 
 def do_click(x_loc, y_loc):
     do_pause(settings.click_delay_min, settings.click_delay_max)
@@ -64,10 +79,11 @@ def do_click(x_loc, y_loc):
         log(f'[[ Clicking ]]: x - {x_loc}, y - {y_loc}')
     p.click(x=x_loc, y=y_loc)
 
+
 def verify(index, action, x, y, a, b, c, notify="False", type="Color"):
     if settings.log_actions:
         log(f'[[ #{index + 1} Verifying ]]: type = {type}, action = {action}, notify = {str(notify)}')
-    
+
     timetocheck = time.time()
     secondstowait = settings.notification_delay
     screenshot_done = False
@@ -77,7 +93,8 @@ def verify(index, action, x, y, a, b, c, notify="False", type="Color"):
     while True:
         color = p.pixel(int(x), int(y))
         time.sleep(0.05)
-        if notify == 'True' and time.time() - timetocheck > secondstowait and timesnotified < timestonotify:
+        if notify == 'True' and time.time(
+        ) - timetocheck > secondstowait and timesnotified < timestonotify:
             if not screenshot_done:
                 do_screenshot()
                 screenshot_done = True
@@ -89,10 +106,13 @@ def verify(index, action, x, y, a, b, c, notify="False", type="Color"):
                 do_click(int(x), int(y))
                 break
         elif type == 'Image':
-            os.makedirs(f'{cwd}/screenshots', exist_ok=True) 
-            s8 = p.locateOnScreen(f'{cwd}/screenshots/{x}.PNG', confidence = 0.98)
+            os.makedirs(f'{cwd}/screenshots', exist_ok=True)
+            s8 = p.locateOnScreen(
+                f'{cwd}/screenshots/{x}.PNG',
+                confidence=0.98)
             if s8 is not None:
                 break
+
 
 def is_float(string):
     try:
@@ -100,6 +120,7 @@ def is_float(string):
         return True
     except ValueError:
         return False
+
 
 def act():
     global settings_read
@@ -114,8 +135,9 @@ def act():
         elif action[0] != '#' and action.split()[0] != "settings":
             settings_read = True
             verify(index, *action.split(' '))
-        elif settings.log_comments:
+        elif action[0] == '#' and settings.log_comments:
             log(action)
+
 
 if __name__ == '__main__':
     read()
--- app/record.py
+++ app/record.py
@@ -1,47 +1,76 @@
 import sys
-import time
 import os
-import pyautogui as p
+import pyautogui
+from dotenv import load_dotenv
 from pynput import mouse
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
+from pynput.mouse import Button
+from pynput.keyboard import Key
 
+load_dotenv()
+mouse_listener = None
+keyboard_listener = None
+current_working_dir = os.getcwd()
 actions = [
-    "settings replay_loops 1",
-    "settings log_comments True",
-    "settings log_actions True",
-    "settings log_debug False",
-    "settings click_delay_min 0.1",
-    "settings click_delay_max 0.2",
-    "settings notification_delay 30",
-    "settings notification_loops 5"
+    f'settings replay_loops {os.getenv("REPLAY_LOOPS")}',
+    f'settings log_comments {os.getenv("LOG_COMMENTS")}',
+    f'settings log_actions {os.getenv("LOG_ACTIONS")}',
+    f'settings log_debug {os.getenv("LOG_DEBUGS")}',
+    f'settings click_delay_min {os.getenv("CLICK_DELAY_MIN")}',
+    f'settings click_delay_max {os.getenv("CLICK_DELAY_MAX")}',
+    f'settings notification_delay {os.getenv("NOTIFICATION_DELAY")}',
+    f'settings notification_loops {os.getenv("NOTIFICATION_LOOPS")}'
 ]
-cwd = os.getcwd()
 
-def log(message):
-    print(message)
 
 def output():
-    os.makedirs(f'{cwd}/output', exist_ok=True) 
-    filename = f'{cwd}/output/{sys.argv[1]}.txt'
+    os.makedirs(f'{current_working_dir}/output', exist_ok=True)
+    filename = f'{current_working_dir}/output/{sys.argv[1]}.txt'
     with open(filename, 'w') as f:
         for action in actions:
-            f.write("%s\n" % action)
-    return filename
+            f.write('%s\n' % action)
+    print(f'[[ Recording Generated ]] - [[ {filename} ]]')
+
 
 def on_click(x, y, button, pressed):
-    if button == mouse.Button.left and pressed:
-        color = p.pixel(x, y)
-        comment = f'# action, update comment here'
-        action = f'click {x} {y} {color[0]} {color[1]} {color[2]} False Color'
+    color = pyautogui.pixel(x, y)
+    comment = f'# '
+    action = f'click {x} {y} {color[0]} {color[1]} {color[2]} False Color'
+
+    if button == Button.left and pressed:
         actions.append(comment)
         actions.append(action)
-        log(action)
-    elif button == mouse.Button.right:
-        log(f'[[ Stopping Recording ]] - [[ Right Click Pressed ]]')
-        log(f'[[ Recording Generated ]] - [[ {output()} ]]')
-        return False
+        print(f'<< Recording... >> - << {action} >>')
+    elif button == Button.right and pressed:
+        pass
+
+
+def on_press(key):
+    if key == Key.esc:
+        output()
+        print(f'[[ Exiting... ]]')
+        os._exit(0)
+
+
+def setup_listeners():
+    global mouse_listener, keyboard_listener
+    mouse_listener = MouseListener(on_click=on_click)
+    keyboard_listener = KeyboardListener(on_press=on_press)
+
+
+def start_listeners():
+    mouse_listener.start()
+    keyboard_listener.start()
+
+
+def join_listeners():
+    print(f'[[ Starting Recording ]] - [[ Press "esc" to exit ]]')
+    mouse_listener.join()
+    keyboard_listener.join()
+
 
 if __name__ == '__main__':
-    listener = mouse.Listener(on_click=on_click)
-    log(f'[[ Starting Recording ]] - [[ Right Click to Stop ]]')
-    listener.start()
-    listener.join()
+    setup_listeners()
+    start_listeners()
+    join_listeners()
